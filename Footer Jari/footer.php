646ba76452e423ebdd4e2c0370beb1e009b53046
wbjari <wbjari@gmail.com>
2014-05-16 15:50:24 +0200

    Footer - Aangemaakt (File path naar andere pagina's nog)

---
+++ Footer Jari/footer.php
@@ -0,0 +1,36 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="http://bootswatch.com/cosmo/bootstrap.min.css">
+</head>
+			<footer>
+				<div class="footer-copyright-bar">
+					<span>&copy; Bright-Side 2014</span>
+				</div>
+				<div id="footer-midbar"></div>
+					<div class="footer-bottom">
+						<div class="footer-column">
+							<h4 class="footer-title">Developers</h4>
+							<ul>
+								<li>Naam 1</li>
+								<li>Naam 2</li>
+								<li>Naam 3</li>
+								<li>Naam 4</li>
+							</ul>
+						</div>
+						<div class="footer-column">
+							<h4 class="footer-title">Sitemap</h4>
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Livestream</a></li>
+								<li><a href="#">Resultaten</a></li>
+								<li><a href="#">Finale</a></li>
+							</ul>
+						</div>
+					</div>
+			</footer>
+		</div> <!-- EINDE CONTAINER/WRAPPER -->
+	</body>
+	<script>
+	
+	</script>
+</html>
