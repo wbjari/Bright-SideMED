15295490d2bfbe4ca19b740a18ac3b2d9042fc8b
DanielvanBavel <D179143@edu.rocwb.nl>
2014-05-22 09:28:58 +0200

    knockout page bijgewerkt

---
+++ knockout page/knockout/knockout.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Knockout page</title>
+        <link rel="stylesheet" href="stylesheet/style.css">
+    	<body background="img/background.png">
+    </head>
+    <body>
+    	<div class="wrapper">
+    		<div class="upperblock">
+				<img src="img/cup1.png" class="cup">
+			</div>
+			<form role="form" action="#" method="POST"> 
+				<div class="quarter-finals-left">	
+    				<div class="kwart-links-form">
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 1" name="kwartfinalist1"> </ul> 
+           				<br>
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 2" name="kwartfinalist2"> </ul>
+           				<br> 
+          				<ul> <input type="text" placeholder="Kwart finale kandidaat 3" name="kwartfinalist3"> </ul> 
+          				<br>
+          				<ul> <input type="text" placeholder="Kwart finale kandidaat 4" name="kwartfinalist4"> </ul> 
+    				</div>
+				</div>
+				<div class="semi-finals-left">	
+					<div class="halve-links-form">
+           				<ul> <input type="text" placeholder="Halve finale kandidaat 1" name="halvefinalist1"> </ul> 
+          				<br>
+           				<br>
+           				<br>
+           				<br>
+           				<ul> <input type="text" placeholder="Halve finale kandidaat 2" name="halvefinalist2"> </ul> 
+    				</div> 
+				</div>
+				<div class="finals">
+				   	<div class="finale-form">    	   
+    	    			<input type="text" placeholder="Finale kandidaat 1" name="finalist1">
+    	    			<input type="text" placeholder="Finale kandidaat 2" name="finalist2">
+    				</div> 	
+				</div>
+				<div class="semi-finals-right">
+					<div class="halve-rechts-form">
+           				<ul> <input type="text" placeholder="Halve finale kandidaat 3" name="halvefinalist3"> </ul> 
+           				<br>
+           				<br>
+           				<br>
+           				<br>
+           				<ul> <input type="text" placeholder="Halve finale kandidaat 4" name="halvefinalist4"> </ul> 
+    				</div>
+    			</div>
+				<div class="quarter-finals-right">	
+					<div class="kwart-rechts-form">
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 5" name="kwartfinalist5"> </ul> 
+           				<br>
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 6" name="kwartfinalist6"> </ul>
+           				<br> 
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 7" name="kwartfinalist7"> </ul> 
+           				<br>
+           				<ul> <input type="text" placeholder="Kwart finale kandidaat 8" name="kwartfinalist8"> </ul> 
+        			</div>
+				</div>
+			</form>				
+		</div>
+    </body>
+</html>		
