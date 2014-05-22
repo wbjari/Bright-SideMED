b18b0adfd14fc61e8feb4877c0b1373bb5c2849f
wbjari <wbjari@gmail.com>
2014-05-22 12:16:16 +0200

    Gekleurde score in code toegevoegd - now_playing.php

--- Documentatie 2/fifadev/med_app/stats/now_playing.php
+++ Documentatie 2/fifadev/med_app/stats/now_playing.php
@@ -8,17 +8,26 @@ $query = mysqli_query($con, $sql);
 if ( mysqli_num_rows($query) == 1 ) {
     $row = mysqli_fetch_assoc($query);
     echo '<tr class="now">';
-    echo '<td>' . $row['wedstrijdnr'] . '</td>';
+    // echo '<td>' . $row['wedstrijdnr'] . '</td>';
     echo '<td>  ' . $row['slot_1'] . '</td>';
-    echo '<td>' . $row['goals_slot_1'] . '</td>';
-    echo '<td> - </td>';
-    echo '<td>' . $row['goals_slot_2'] . '</td>';
+    if ($row['goals_slot_1'] > $row['goals_slot_2']) {
+    	echo "<td id='goals_slot_1' class='text-success'>".$row['goals_slot_1']."</td>";
+    	echo "<td> - </td>";
+    	echo "<td id='goals_slot_2' class='text-danger'>".$row['goals_slot_2']."</td>";
+    } elseif ($row['goals_slot_1'] < $row['goals_slot_2']) {
+    	echo "<td id='goals_slot_1' class='text-danger'>".$row['goals_slot_1']."</td>";
+    	echo "<td> - </td>";
+    	echo "<td id='goals_slot_2' class='text-success'>".$row['goals_slot_2']."</td>";
+    } else {
+    	echo "<td id='goals_slot_1' class='text-warning'>".$row['goals_slot_1']."</td>";
+    	echo "<td> - </td>";
+    	echo "<td id='goals_slot_2' class='text-warning'>".$row['goals_slot_2']."</td>";
+    }
     echo '<td>' . $row['slot_2'] . '</td>';
     echo '</tr>'; 
 }  else {
 echo 'no rows found...';
 }
-
 
 
 
